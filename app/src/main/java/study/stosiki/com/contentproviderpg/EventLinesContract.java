0b4440d6b4f7716031c78a2a636c11daa56688b5
mikem <mikemeyer28@yahoo.com>
2015-07-21 20:01:23 +0200

    none

--- app/src/main/java/study/stosiki/com/contentproviderpg/EventLinesContract.java
+++ app/src/main/java/study/stosiki/com/contentproviderpg/EventLinesContract.java
@@ -14,24 +14,66 @@ public class EventLinesContract {
     /* content URI for the top-level eventlines authority */
     public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY);
 
+    private static final String MIME_SUBTYPE_EVENT = "/vnd.com.stosiki.event";
+    private static final String MIME_SUBTYPE_EVENTLINE = "/vnd.com.stosiki.eventline";
+    private static final String MIME_SUBTYPE_EVENTLINE_LIST_ITEM =
+            "/vnd.com.stosiki.eventline_list_item";
+
     /* selection clause for ID based queries */
     public static final String SELECTION_ID_BASED = BaseColumns._ID + " = ? ";
 
     public static final class Events {
         /* content URI for the events table */
         public static final Uri CONTENT_URI =
-                Uri.withAppendedPath(EventLinesContract.CONTENT_URI, "events");
+                Uri.withAppendedPath(EventLinesContract.CONTENT_URI, DbSchema.TBL_EVENTS);
         /* directory MIME type */
         public static final String CONTENT_TYPE =
-                ContentResolver.CURSOR_DIR_BASE_TYPE + "/vnd.com.stosiki.eventlines";
+                ContentResolver.CURSOR_DIR_BASE_TYPE + MIME_SUBTYPE_EVENT;
         /* single item type */
         public static final String CONTENT_ITEM_TYPE =
-                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/vnd.com.stosiki.eventlines";
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + MIME_SUBTYPE_EVENT;
         /* projection of all columns in the events table */
-        public static final String[] PROJECTION_ALL = {}
+        public static final String[] PROJECTION_ALL = {
+                DbSchema.COL_ID,
+                DbSchema.COL_TIMESTAMP,
+                DbSchema.COL_LINE_ID
+        };
     }
 
     public static final class EventLines {
+        /* content URI for the event_lines table */
+        public static final Uri CONTENT_URI =
+                Uri.withAppendedPath(EventLinesContract.CONTENT_URI, DbSchema.TBL_EVENT_LINES);
+        /* directory MIME type */
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + MIME_SUBTYPE_EVENTLINE;
+        /* single item type */
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + MIME_SUBTYPE_EVENTLINE;
+        /* projection of all columns in the event_lines table */
+        public static final String[] PROJECTION_ALL = {
+                DbSchema.COL_ID,
+                DbSchema.COL_LINE_TYPE,
+                DbSchema.COL_TITLE
+        };
+    }
 
+    /* constants for a join of events and event_lines tables, used by listview */
+    public static final class EventLineListItem {
+        /* content URI for the event_lines table */
+        public static final Uri CONTENT_URI =
+                Uri.withAppendedPath(EventLinesContract.CONTENT_URI, DbSchema.TBL_EVENT_LINE);
+        /* directory MIME type */
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + MIME_SUBTYPE_EVENTLINE;
+        /* single item type */
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + MIME_SUBTYPE_EVENTLINE;
+        /* projection of all columns in the event_lines table */
+        public static final String[] PROJECTION_ALL = {
+                DbSchema.COL_ID,
+                DbSchema.COL_LINE_TYPE,
+                DbSchema.COL_TITLE
+        };
     }
 }
